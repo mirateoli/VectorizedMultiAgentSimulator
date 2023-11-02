962a3e19e7439195c5e6b32ac935c14f9e5d6ec8
mirateoli <79615899+mirateoli@users.noreply.github.com>
2023-11-02 09:57:09 -0400

    Fixed bugs
    1) Fixed observation return
    2) Added dtype=torch.float32 to start_pos and goals tensors (defaulted to long for some reason)

--- mira/line_routing.py
+++ mira/line_routing.py
@@ -1,6 +1,10 @@
 from torchrl.envs.libs.vmas import VmasEnv
 from torchrl.envs.utils import check_env_specs
 
+from tensordict import TensorDict
+from tensordict.nn import TensorDictModule
+from tensordict.nn.distributions import NormalParamExtractor
+
 from vmas.simulator.scenario import BaseScenario
 
 import typing
@@ -25,18 +29,18 @@ if typing.TYPE_CHECKING:
     from vmas.simulator.rendering import Geom
 
 class Scenario(BaseScenario):
-     def make_world(self, batch_dim: int, device: torch.device, **kwargs) -> World:
+    def make_world(self, batch_dim: int, device: torch.device, **kwargs) -> World:        
         # pass any kwargs that we need
         # n_agents is num of pipes to route, default is 2
         self.n_agents = kwargs.get("n_agents", 1)
         # set collision bool
-        self.collisions = kwargs.get("collisions", True)
+        self.collisions = kwargs.get("collisions", False)
         # set start position per agent
         start_pos = kwargs.get("start_pos",[(0,0)])
-        self.start_pos = torch.tensor(start_pos)
+        self.start_pos = torch.tensor(start_pos, dtype=torch.float32)
         # set goals per agent
         goals = kwargs.get("goals",[(1,1)])
-        self.goals = torch.tensor(goals)
+        self.goals = torch.tensor(goals, dtype=torch.float32)
 
         # create world
         world = World(batch_dim, device, dt=0.1, drag = 0)
@@ -64,45 +68,41 @@ class Scenario(BaseScenario):
      
      
 
-     def reset_world_at(self, env_index: int = None):
+    def reset_world_at(self, env_index: int = None):
         for i, agent in enumerate(self.world.agents):
             agent.set_pos(self.start_pos[i], batch_index=env_index)
             agent.goal.set_pos(self.goals[i], batch_index=env_index)
 
         
 
-     def observation(self, agent: Agent) -> AGENT_OBS_TYPE:
+    def observation(self, agent: Agent) -> AGENT_OBS_TYPE:
         # get position of all entities in this agent's reference frame
-        goal_dist = []
-        goal_dist.append(agent.state.pos - agent.goal.state.pos)
+        # goal_dist = []
+        # goal_dist.append(agent.state.pos - agent.goal.state.pos)
         return torch.cat([
                 agent.state.pos,
                 agent.state.vel,
-            ]
-            + goal_dist)
+                agent.state.pos - agent.goal.state.pos
+            ],
+            dim=-1
+        )
+            
      
-     def reward(self, agent: Agent) -> AGENT_REWARD_TYPE:
+    def reward(self, agent: Agent) -> AGENT_REWARD_TYPE:
         # reward every agent proportionally to distance from their goal
         dist_rew = torch.sum(
-            torch.square(agent.state.pos - agent.goal.state.pos, dim=-1)
+            torch.square(agent.state.pos - agent.goal.state.pos), dim=-1
         )
         return -dist_rew
      
-     def done(self):
-        return torch.stack(
-            [
-                torch.linalg.vector_norm(
-                    agent.state.pos - agent.goal.state.pos,
-                    dim=-1,
-                )
-                < agent.shape.radius
-                for agent in self.world.agents
-            ],
-            dim=-1,
-        ).all(-1)
+    def done(self):
+
+        return torch.tensor([False], device=self.world.device).expand(
+            self.world.batch_dim
+        )
      
-     def info(self, agent: Agent) -> AGENT_INFO_TYPE:
-        return {}
+    # def info(self, agent: Agent) -> AGENT_INFO_TYPE:
+    #     return {}
      
 
 start_pos = [(0,0),(0,1)]
@@ -111,17 +111,35 @@ scenario = Scenario()
 
 env = VmasEnv(
     scenario=scenario, 
-    num_envs=32, 
+    num_envs=6, 
     n_agents=2, 
     start_pos = start_pos,
     goals = goals)
-# print(env.start_pos)
-print("action_spec:", env.full_action_spec)
-print("reward_spec:", env.full_reward_spec)
-print("done_spec:", env.full_done_spec)
-print("observation_spec:", env.observation_spec)
 
-print("action_keys:", env.action_keys)
-print("reward_keys:", env.reward_keys)
-print("done_keys:", env.done_keys)
+
+# print("action_spec:", env.full_action_spec)
+# print("reward_spec:", env.full_reward_spec)
+# print("done_spec:", env.full_done_spec)
+# print("observation_spec:", env.observation_spec)
+
+# print("action_keys:", env.action_keys)
+# print("reward_keys:", env.reward_keys)
+# print("done_keys:", env.done_keys)
+
+
+
+check_env_specs(env)
      
+
+n_rollout_steps = 5
+rollout = env.rollout(n_rollout_steps)
+print("rollout of three steps:", rollout)
+print("Shape of the rollout TensorDict:", rollout.batch_size)
+
+with torch.no_grad():
+   env.rollout(
+       max_steps=1000,
+       callback=lambda env, _: env.render(),
+       auto_cast_to_device=True,
+       break_when_any_done=False,
+   )
--- mira/test.py
+++ mira/test.py
@@ -1,8 +1,15 @@
 import torch
 from torch import Tensor
 
-list = [(0,0),(1,0)]
-tensor = torch.tensor(list)
+list = [(0,0),(2,0)]
+tensor = torch.tensor(list, dtype=torch.float32)
 
 print("list:", list)
 print("tensor:", tensor[1])
+
+v = torch.linalg.vector_norm(tensor,dim=1)
+print(v)
+
+sq = torch.sum(
+    torch.square(tensor), dim=1)
+print(sq)
